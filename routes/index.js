b19c9438195d63df25f9db50e8833fbd402a8262
WilliamTsou818 <cccc1589@gmail.com>
2021-06-30 21:44:18 +0800

    feat: add user status authenticator to route

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ const restController = require('../controllers/restController')
 const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) return next()
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   // index route
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
   // admin route
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   // signup route
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
