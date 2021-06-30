b5e4d253122dc0121675f4ff19a3a20adfc91b33
WilliamTsou818 <cccc1589@gmail.com>
2021-06-30 20:47:29 +0800

    feat: modify userController to check user password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,6 +9,19 @@ const userController = {
   signUp: (req, res) => {
     const name = req.body.name
     const email = req.body.email
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '確認密碼與密碼不相符')
+      return res.redirect('/signup')
+    }
+    // confirm unique user
+    User.findOne({ where: { email: email } }).then(user => {
+      if (user) {
+        req.flash('error_messages', '該信箱已被註冊')
+        return res.redirect('/signup')
+      }
+    })
+
     return bcrypt
       .genSalt(10)
       .then(salt => bcrypt.hash(req.body.password, salt))
