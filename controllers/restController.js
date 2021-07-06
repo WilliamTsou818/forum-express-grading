3a25207a9f18321519e8a38ee84ec56f778cd03c
WilliamTsou818 <cccc1589@gamil.com>
2021-07-06 12:44:35 +0800

    feat: add categories navbar in restaurant index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,10 +4,17 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
+    const whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery.CategoryId = categoryId
+    }
     Restaurant.findAll({
       raw: true,
       nest: true,
-      include: [Category]
+      include: [Category],
+      where: whereQuery
     })
       .then(restaurants => {
         const restaurantData = restaurants.map(r => ({
@@ -15,7 +22,13 @@ const restController = {
           description: r.description.substring(0, 50),
           categoryName: r.Category.name
         }))
-        return res.render('restaurants', { restaurants: restaurantData })
+        Category.findAll({ raw: true, nest: true }).then(categories => {
+          return res.render('restaurants', {
+            restaurants: restaurantData,
+            categories: categories,
+            categoryId: categoryId
+          })
+        })
       })
   },
   getRestaurant: (req, res) => {
