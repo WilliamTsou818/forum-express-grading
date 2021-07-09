bb924d872e988bafe6920cb09e81344725c6b162
WilliamTsou818 <cccc1589@gamil.com>
2021-07-09 17:40:57 +0800

    feat: A22 top 10 restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -116,6 +116,24 @@ const restController = {
           commentCount: comments.count
         })
       })
+  },
+  getTopRestaurant: (req, res) => {
+    return Restaurant.findAll({
+      include: [
+        { model: User, as: 'FavoritedUsers' }
+      ]
+    })
+      .then(restaurants => {
+        restaurants = restaurants.map(restaurant => ({
+          ...restaurant.dataValues,
+          description: restaurant.description.slice(0, 50),
+          favoriteCounts: restaurant.FavoritedUsers.length,
+          isFavorited: restaurant.FavoritedUsers.map(d => d.id).includes(helpers.getUser(req).id)
+        }))
+        restaurants.sort((a, b) => b.favoriteCounts - a.favoriteCounts)
+        restaurants = restaurants.slice(0, 10)
+        return res.render('topRestaurant', { restaurants })
+      })
   }
 }
 
