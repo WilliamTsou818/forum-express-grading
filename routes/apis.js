46455fbd1a164d614bc70e6f395737f0df1297f1
WilliamTsou818 <cccc1589@gamil.com>
2021-07-13 14:02:06 +0800

    feat: add GET /api/admin/categories

--- routes/apis.js
+++ routes/apis.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../controllers/api/adminController.js')
+const categoryController = require('../controllers/api/categoryController')
+const categoryService = require('../services/categoryService.js')
 
+// admin restaurants
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
+
+// admin categories
+router.get('/admin/categories', categoryController.getCategories)
 
 module.exports = router
